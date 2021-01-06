3f0c04b9befd7405dbdfd53130cb25851a3a25cb
Anton Zaparivannyi <zaparivannyi@gmail.com>
2021-01-07 00:32:54 +0300

    Страница списка фильмов вынесена в отдельный компонент и параметризирована спском фильмов. Теперь она вызывается на странице фильмов и на странице лайкнутых фильмов

--- src/app/components/films-page/films-page.module.ts
+++ src/app/components/films-page/films-page.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FilmsPageComponent} from './films-page.component';
-import { FilmListItemComponent } from './film-list-item/film-list-item.component';
 import {RouterModule} from '@angular/router';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
 import {SharedModule} from '../../shared/shared.module';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 
 
 @NgModule({
-  declarations: [FilmsPageComponent, FilmListItemComponent],
+  declarations: [FilmsPageComponent],
   imports: [
     CommonModule,
     RouterModule,
-    MatCardModule,
-    MatButtonModule,
+    MatProgressSpinnerModule,
     SharedModule
   ],
   exports: [FilmsPageComponent]
--- src/app/components/liked-films-page/liked-films-page.component.ts
+++ src/app/components/liked-films-page/liked-films-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ThemeService} from '../../services/theme.service';
+import Film from '../../models/film';
+import {FilmHttpService} from '../../services/api/http/film-http.service';
 
 @Component({
   selector: 'app-liked-films-page',
@@ -6,11 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./liked-films-page.component.scss']
 })
 export class LikedFilmsPageComponent implements OnInit {
+  public films: Film[];
   public isLoading: boolean = false;
 
-  constructor() { }
+  constructor(private themeService: ThemeService,
+              private filmHttpService: FilmHttpService) { }
 
   ngOnInit(): void {
+    this.isLoading = true;
+    this.filmHttpService.getLikedFilms().subscribe(
+      films => {
+        this.films = films;
+        this.isLoading = false;
+      },
+      (err) => {
+        console.error(err);
+        this.isLoading = false;
+      }
+    );
   }
 
 }
