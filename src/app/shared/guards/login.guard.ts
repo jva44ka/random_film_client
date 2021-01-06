4d4d5577e7790126c8a7e3800ff54314a1510878
Anton Zaparivannyi <zaparivannyi@gmail.com>
2021-01-06 22:23:52 +0300

    Добавление редиректа в гвард и фикс ошибки

--- src/app/shared/guards/login.guard.ts
+++ src/app/shared/guards/login.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import {AuthService} from '../../services/auth.service';
 import {Guid} from 'guid-typescript';
@@ -8,11 +8,19 @@ import {Guid} from 'guid-typescript';
   providedIn: 'root'
 })
 export class LoginGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService,
+              private router: Router) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return Guid.isGuid(Guid.parse(this.authService.getUserId()));
+    let isGuid: boolean = false;
+    try {
+      isGuid = Guid.isGuid(Guid.parse(this.authService.getUserId()));
+    } catch(err) {
+      console.warn(err);
+      this.router.navigate(['']);
+    }
+    return isGuid;
   }
 }
