acc815a834de951962630f8b8d879b76b2cd210a
Anton Zaparivannyi <zaparivannyi@gmail.com>
2021-01-06 21:50:33 +0300

    Сделана основная верстка страницы профиля (пока без возможности изменения данных)

--- src/app/components/profile-page/profile-page.component.ts
+++ src/app/components/profile-page/profile-page.component.ts
@@ -1,25 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {UserHttpService} from '../../services/api/http/user-http.service';
 import {AuthService} from '../../services/auth.service';
 import {LoginResult} from '../../models/result-models/login-result';
 import {Router} from '@angular/router';
 import User from '../../models/user';
+import {ThemeService} from '../../services/theme.service';
 
 @Component({
   selector: 'profile-page',
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.scss']
 })
-export class ProfilePageComponent implements OnInit {
+export class ProfilePageComponent implements OnInit, OnDestroy {
   public isLoading: boolean = false;
   public account: User;
+
+  public get avatarUrl(): string {
+    return this.account.avatarUrl ?
+      `url(${this.account.avatarUrl})` :
+      'url(assets/image_not_found.png)'
+  }
 
   private subscriptions: Subscription[] = [];
 
   constructor(private userHttpService: UserHttpService,
               private authService: AuthService,
-              private router: Router) { }
+              private router: Router,
+              private themeService: ThemeService) { }
 
   ngOnInit(): void {
     this.subscriptions.push(
@@ -43,4 +51,9 @@ export class ProfilePageComponent implements OnInit {
     );
   }
 
+  ngOnDestroy(): void {
+    for (let sub of this.subscriptions) {
+      sub?.unsubscribe();
+    }
+  }
 }
